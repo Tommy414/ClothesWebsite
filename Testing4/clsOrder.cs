d66f85fd980531e3d0bd424bdcedbdad14d3d4ff
Gradi <p2644927@GWHXX284WIN007W>
2024-05-02 16:31:41 +0100

    Test

---
+++ Testing4/clsOrder.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class clsOrder
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsOrder anOrder = new clsOrder();
+            Assert.IsNotNull(anOrder);
+        }
+    }
+}
