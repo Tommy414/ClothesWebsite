5ee11caacfcca1ac343f6333fae81d6809c6572d
Tommy <p2774538@GWHXX284WIN013W>
2024-05-02 16:42:29 +0100

    Testing

---
+++ Testing1/clsStaff.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class clsStaff
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsStaff s = new clsStaff();
+            Assert.IsNotNull(s);
+        }
+    }
+}
